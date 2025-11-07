42f31a20c0c64d192f8ecf7cb4591a9223fca6a1
Thanmai sree Macharla <145900938+thanmai09@users.noreply.github.com>
2025-11-07 07:53:58 +0530

    Time: 0 ms (100%), Space: 43.1 MB (5.05%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.java
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -1,20 +1,20 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int ans = 0;
-        for (int i = 0;i<stones.length();i++){
+        for(int i = 0;i < stones.length();i++){
             char ch = stones.charAt(i);
             boolean found = false;
-            for (int j = 0; j< jewels.length();j++){
-                char die = jewels.charAt(j);
-                if(ch == die){
-                    found = true;
-                    break;
-                }
+           for(int j = 0;j<jewels.length();j++){
+            char dia = jewels.charAt(j);
+            if(dia == ch){
+                found = true;
             }
-            if (found == true){
-                ans += 1;
-            }
+           }
+           if (found){
+            ans += 1;
+           }
         }
-        return ans;        
+    return ans;
     }
+    
 }
