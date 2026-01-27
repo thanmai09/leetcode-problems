08c5f743e47c8baf108cf4174ab14e02f66b0321
Thanmai sree Macharla <145900938+thanmai09@users.noreply.github.com>
2026-01-27 20:41:35 +0530

    Time: 0 ms (100%), Space: 19.3 MB (32.57%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        
+        prev2, prev1 = 1, 2  # ways to reach step 1 and 2
+        
+        for _ in range(3, n + 1):
+            curr = prev1 + prev2
+            prev2 = prev1
+            prev1 = curr
+        
+        return prev1
+
+        
